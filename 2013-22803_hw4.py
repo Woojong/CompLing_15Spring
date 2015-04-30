5e85425b71bd281fafdf368e730c5cf1bc9b0511
APD <apeedee.w@gmail.com>
2015-04-30 11:56:44 +0900

    unk process modified

--- 2013-22803_hw4.py
+++ 2013-22803_hw4.py
@@ -7,7 +7,8 @@ __date__ = '2015-05-05'
 과제 4:교차엔트로피(cross entropy)를 통한 언어 모델(Language model) 비교:
 (3명까지의 그룹 가능, 5월 6일까지 ETL에 탑재)
 sejong.nov.train은 세종코퍼스에서 추출한 소설코퍼스이다. 이 코퍼스에서 자소별, 음절별로 유니그램, 바이그램 언어 모델을 설정하여,
-역시 세종코퍼스의 소설 코퍼스인 sejong.nov.test와 한겨레 신문 기사인 hani.test의 교차엔트로피를 계산하여 어느 모델이 더 좋은 모델인지를 살펴보자. (training의 확률을 곱해서, p(x)logm(x))
+역시 세종코퍼스의 소설 코퍼스인 sejong.nov.test와 한겨레 신문 기사인 hani.test의 교차엔트로피를 계산하여 어느 모델이 더 좋은 모델인지를 살펴보자.
+(training의 확률을 곱해서, p(x)logm(x))
 이를 다음의 테이블을 출력하여 그 결과를 보여라.
 (test에서 나타나고 training에서 나타나지 않으면 unknown으로 할 것)
 * 모든 파일은 utf8으로 인코딩 되어 있다.
@@ -59,24 +60,32 @@ def sylTojamo(syllable_list): # syllable to jamo
                 tmp_list.append(decoded)
     return tmp_list
 
-def UNK_process(traindata, testdata):
-    keylist = testdata.keys()
+#### Converting unseen words in trainig data to <UNK>
+def UNK_process(traindata, testdata, trainngram, testngram): # traindata for training probability data, testdata for test probability data, trainngram for training n-gram dictionary, testgram for test n-gram dictionary
+    keylist = testdata.keys() # extracting test data probability dictionary keys
+    total_unk = 0 # setting UNK probability as 0
+    train_nvocab = sum(trainngram.values())
     for keys in keylist:
         try:
             traindata[keys]
         except:
-            testdata[u"<UNK>"] = testdata[u"<UNK>"] + testdata[keys] if u"<UNK>" in testdata.keys() else testdata[keys]
-    return testdata
+            total_unk = total_unk + testngram[keys] if u"<UNK>" in trainngram.keys() else testngram[keys]
+    unk_prob = float(total_unk)/float(train_nvocab)
+    return unk_prob
 
-def entropy_compute(data, crossdata, dict = True): # if you using cross entropy, data for training data, crossdata for test data, if you want to just entropy calculating, then data and crossdata for same data set (e.g., data = test, crossdata = test)
-    from math import log
-    def log2(x): # for using log base 2 as function
+#### Computing entropy (and cross entropy, if you want entroypy: "data == crossdata", cross entropy: "data != crossdata")
+def entropy_compute(data, crossdata, unkprob = None, dict = True): # if you using cross entropy, data for training data, crossdata for test data, if you want to just entropy calculating, then data and crossdata for same data set (e.g., data = test, crossdata = test)
+    from math import log # for using log base 2 as function
+    def log2(x):
         return log(x, 2)
-    entropy_dict = {}
-    data_keylist = data.keys()
+    entropy_dict = {} # empty entropy dictionary
+    data_keylist = data.keys() # extract data dictionary keys
     for keylist in data_keylist:
-        entropy_dict[keylist] = data[keylist]*log2(crossdata[keylist])
-    entropy_values = sum(entropy_dict.values())
+        try:
+            entropy_dict[keylist] = data[keylist]*log2(crossdata[keylist]) # p*log_2(p)
+        except:
+            entropy_dict[keylist] = data[keylist]*log2(unkprob) # if don't exist intraining data, then UNK probability which was computed already
+    entropy_values = sum(entropy_dict.values()) # summation of entropy of data
     if dict:
         return entropy_dict
     elif dict == False:
@@ -104,38 +113,39 @@ train_bijamo_dict = ngram_maker(train_jamo, 2)
 train_unisyl_dict = ngram_maker(train_syllable, 1)
 train_bisyl_dict = ngram_maker(train_syllable, 2)
 
-test1_unisyl_unkdict = UNK_process(train_unisyl_dict, test1_unisyl_dict)
-test1_bisyl_unkdict = UNK_process(train_bisyl_dict, test1_bisyl_dict)
-test1_unijamo_unkdict = UNK_process(train_unijamo_dict, test1_unijamo_dict)
-test1_bijamo_unkdict = UNK_process(train_bijamo_dict, test1_bijamo_dict)
-test2_unisyl_unkdict = UNK_process(train_unisyl_dict, test2_unisyl_dict)
-test2_bisyl_unkdict = UNK_process(train_bisyl_dict, test2_bisyl_dict)
-test2_unijamo_unkdict = UNK_process(train_unijamo_dict, test2_unijamo_dict)
-test2_bijamo_unkdict = UNK_process(train_bijamo_dict, test2_bijamo_dict)
 
 train_unisylprob = cond_prob(train_unisyl_dict, [], True)
 train_bisylprob = cond_prob(train_bisyl_dict, train_unisyl_dict)
 train_unijamoprob = cond_prob(train_unijamo_dict, [], True)
 train_bijamoprob = cond_prob(train_bijamo_dict, train_unijamo_dict)
-test1_unisylprob = cond_prob(test1_unisyl_unkdict, [], True)
-test1_bisylprob = cond_prob(test1_bisyl_unkdict, test1_unisyl_unkdict)
-test1_unijamoprob = cond_prob(test1_unijamo_unkdict, [], True)
-test1_bijamoprob = cond_prob(test1_bijamo_unkdict, test1_unijamo_unkdict)
-test2_unisylprob = cond_prob(test2_unisyl_unkdict, [], True)
-test2_bisylprob = cond_prob(test2_bisyl_unkdict, test2_unisyl_unkdict)
-test2_unijamoprob = cond_prob(test2_unijamo_unkdict, [], True)
-test2_bijamoprob = cond_prob(test2_bijamo_unkdict, test2_unijamo_unkdict)
+test1_unisylprob = cond_prob(test1_unisyl_dict, [], True)
+test1_bisylprob = cond_prob(test1_bisyl_dict, test1_unisyl_dict)
+test1_unijamoprob = cond_prob(test1_unijamo_dict, [], True)
+test1_bijamoprob = cond_prob(test1_bijamo_dict, test1_unijamo_dict)
+test2_unisylprob = cond_prob(test2_unisyl_dict, [], True)
+test2_bisylprob = cond_prob(test2_bisyl_dict, test2_unisyl_dict)
+test2_unijamoprob = cond_prob(test2_unijamo_dict, [], True)
+test2_bijamoprob = cond_prob(test2_bijamo_dict, test2_unijamo_dict)
 
-entropy_compute(train_unisylprob, train_unisylprob, dict = False)
-entropy_compute(train_bisylprob, train_bisylprob, dict = False)
-entropy_compute(train_unisylprob, test1_unisylprob, dict = False)
-entropy_compute(train_unisylprob, test2_unisylprob, dict = False)
-entropy_compute(train_bisylprob, test1_bisylprob, dict = False)
-entropy_compute(train_bisylprob, test2_bisylprob, dict = False)
 
-entropy_compute(train_unijamoprob, train_unijamoprob, dict = False)
-entropy_compute(train_bijamoprob, train_bijamoprob, dict = False)
-entropy_compute(train_unijamoprob, test1_unijamoprob, dict = False)
-entropy_compute(train_unijamoprob, test2_unijamoprob, dict = False)
-entropy_compute(train_bijamoprob, test1_bijamoprob, dict = False)
-entropy_compute(train_bijamoprob, test2_bijamoprob, dict = False)
+unisyl_entropy = entropy_compute(train_unisylprob, train_unisylprob, dict = False)
+bisyl_entropy = entropy_compute(train_bisylprob, train_bisylprob, dict = False)
+train1_unisyl_unkprob = UNK_process(train_unisylprob, test1_unisylprob, train_unisyl_dict, test1_unisyl_dict)
+unisyl1_cross_ent = entropy_compute(test1_unisylprob, train_unisylprob, train1_unisyl_unkprob, dict = False)
+train2_unisyl_unkprob = UNK_process(train_unisylprob, test2_unisylprob, train_unisyl_dict, test2_unisyl_dict)
+unisyl2_cross_ent = entropy_compute(test2_unisylprob, train_unisylprob, train2_unisyl_unkprob, dict = False)
+train1_bisyl_unkprob = UNK_process(train_bisylprob, test1_bisylprob, train_bisyl_dict, test1_bisyl_dict)
+bisyl1_cross_ent = entropy_compute(test1_bisylprob, train_bisylprob, train1_bisyl_unkprob, dict = False)
+train2_bisyl_unkprob = UNK_process(train_bisylprob, test2_bisylprob, train_bisyl_dict, test2_bisyl_dict)
+bisyl2_cross_ent = entropy_compute(test2_bisylprob, train_bisylprob, train2_bisyl_unkprob, dict = False)
+
+unijamo_entropy = entropy_compute(train_unijamoprob, train_unijamoprob, dict = False)
+bijamo_entropy = entropy_compute(train_bijamoprob, train_bijamoprob, dict = False)
+train1_unijamo_unkprob = UNK_process(train_unijamoprob, test1_unijamoprob, train_unijamo_dict, test1_unijamo_dict)
+unijamo1_cross_ent = entropy_compute(test1_unijamoprob, train_unijamoprob, train1_unijamo_unkprob, dict = False)
+train2_unijamo_unkprob = UNK_process(train_unijamoprob, test2_unijamoprob, train_unijamo_dict, test2_unijamo_dict)
+unijamo2_cross_ent = entropy_compute(test2_unijamoprob, train_unijamoprob, train2_unijamo_unkprob, dict = False)
+train1_bijamo_unkprob = UNK_process(train_bijamoprob, test1_bijamoprob, train_bijamo_dict, test1_bijamo_dict)
+bijamo1_cross_ent = entropy_compute(test1_bijamoprob, train_bijamoprob, train1_bijamo_unkprob, dict = False)
+train2_bijamo_unkprob = UNK_process(train_bijamoprob, test2_bijamoprob, train_bijamo_dict, test2_bijamo_dict)
+bijamo2_cross_ent = entropy_compute(test2_bijamoprob, train2_bijamo_unkprob, train_bijamoprob, dict = False)
