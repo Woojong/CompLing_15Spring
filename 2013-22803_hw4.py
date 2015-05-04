1520427c6d0547905f16106449138747fe70f4d5
APD <apeedee.w@gmail.com>
2015-05-05 00:01:21 +0900

    final ver 1.0

--- 2013-22803_hw4.py
+++ 2013-22803_hw4.py
@@ -39,6 +39,39 @@ f = codecs.open(curr_dir + my_dir + filename[2], "r", "utf-8")
 test2 = f.read()
 f.close()
 
+#### Defining function for making n-gram list
+def ngram_maker(data, n_digit, dict=True): #data for word corpus (post-processed), n_digit for the number of "n"-gram
+    tmp_ngram = [] #Creating empty temporary n-gram list
+    for num in range(len(data)-n_digit+1): # make n-gram list using list type e.g., [['The', 'last'], ['last', 'forever']]
+        tmp_ngram.append(data[num:num+n_digit]) # from num to num+n_digit e.g., data[0:2], data[1:3]
+    ngram_list = [] # creating empty n-gram list and n-gram dict
+    ngram_dict = {}
+    for unlist in range(len(tmp_ngram)): # Unlisting n-gram list e.g., ['The last'], ['last forever']
+        tmp_words = " ".join(tmp_ngram[unlist]) # join each list elements with " "(space)
+        ngram_list.append(tmp_words) # appending n-gram list to joined words
+        ngram_dict.setdefault(tmp_words,0) # making default dictionary value for current words or zero
+        ngram_dict[tmp_words] += 1 # plus 1 if exist current words
+    if dict == True:
+        return ngram_dict # returning dictionary
+    else:
+        return ngram_list # returning list
+
+#### calculating conditional probability defining function
+def cond_prob(numer_dict, denom_dict, ngramprob = False): # numer_dict for numerator dictionary, denom_dict for denominator dictionary, ngramprob for n-gram probability not conditional probability
+    conditional = {} # initializing conditional probability dictionary
+    ngram = {} # initializing n-gram probability dictionary
+    total_numer = sum(numer_dict.values())
+    for key in numer_dict.keys():
+        cur_ngram = key.split() # load ngram dict by dict
+        conditional_prob = float(numer_dict[key])/float(denom_dict[" ".join(cur_ngram[:len(cur_ngram)-1])]) # calculating ngram dict probability using c(n-gram)/c((n-1)-gram)
+        ngram_prob = float(numer_dict[key])/float(total_numer) # n-gram probability e.g., c(w1 w2)/sum(c(w1 w2))
+        conditional[key] = conditional_prob # assigning conditional probability
+        ngram[key] = ngram_prob # assigning n-gram probability
+    if ngramprob == False:
+        return conditional # returing conditiaonl probability
+    else:
+        return ngram # returning n-gram probability
+
 #### Filtering only hangul syllable
 def onlyHangul(data):
     tmp_list = []
@@ -89,15 +122,17 @@ def entropy_compute(data, crossdata, unkprob = None, dict = True): # if you usin
     elif dict == False:
         return entropy_values
 
+# including only hangule
 test1_syllable = onlyHangul(test1)
 test2_syllable = onlyHangul(test2)
 train_syllable = onlyHangul(train)
 
+# convert syllable (eumjeol) to jamo
 test1_jamo = sylTojamo(test1_syllable)
 test2_jamo = sylTojamo(test2_syllable)
 train_jamo = sylTojamo(train_syllable)
 
-from compling_NLP import ngram_maker, cond_prob
+# making unigram and bigram with test, training data
 test1_unijamo_dict = ngram_maker(test1_jamo, 1)
 test1_bijamo_dict = ngram_maker(test1_jamo, 2)
 test1_unisyl_dict = ngram_maker(test1_syllable, 1)
@@ -111,7 +146,7 @@ train_bijamo_dict = ngram_maker(train_jamo, 2)
 train_unisyl_dict = ngram_maker(train_syllable, 1)
 train_bisyl_dict = ngram_maker(train_syllable, 2)
 
-
+# calculating MLE probability
 train_unisylprob = cond_prob(train_unisyl_dict, [], True)
 train_bisylprob = cond_prob(train_bisyl_dict, train_unisyl_dict)
 train_unijamoprob = cond_prob(train_unijamo_dict, [], True)
@@ -125,36 +160,47 @@ test2_bisylprob = cond_prob(test2_bisyl_dict, test2_unisyl_dict)
 test2_unijamoprob = cond_prob(test2_unijamo_dict, [], True)
 test2_bijamoprob = cond_prob(test2_bijamo_dict, test2_unijamo_dict)
 
+# entropy calculating
 unisyl_entropy = entropy_compute(train_unisylprob, train_unisylprob, dict = False)
 bisyl_entropy = entropy_compute(train_bisylprob, train_bisylprob, dict = False)
 test1_unisyl_entropy = entropy_compute(test1_unisylprob, test1_unisylprob, dict = False)
 test2_unisyl_entropy = entropy_compute(test2_unisylprob, test2_unisylprob, dict = False)
 test1_bisyl_entropy = entropy_compute(test1_bisylprob, test1_bisylprob, dict = False)
 test2_bisyl_entropy = entropy_compute(test2_bisylprob, test2_bisylprob, dict = False)
+
+# calculating <UNK> probability
 train1_unisyl_unkprob = UNK_process(train_unisylprob, test1_unisylprob, train_unisyl_dict, test1_unisyl_dict)
-unisyl1_cross_ent = entropy_compute(test1_unisylprob, train_unisylprob, train1_unisyl_unkprob, dict = False)
 train2_unisyl_unkprob = UNK_process(train_unisylprob, test2_unisylprob, train_unisyl_dict, test2_unisyl_dict)
-unisyl2_cross_ent = entropy_compute(test2_unisylprob, train_unisylprob, train2_unisyl_unkprob, dict = False)
 train1_bisyl_unkprob = UNK_process(train_bisylprob, test1_bisylprob, train_bisyl_dict, test1_bisyl_dict)
-bisyl1_cross_ent = entropy_compute(test1_bisylprob, train_bisylprob, train1_bisyl_unkprob, dict = False)
 train2_bisyl_unkprob = UNK_process(train_bisylprob, test2_bisylprob, train_bisyl_dict, test2_bisyl_dict)
+
+# calculating cross entropy with train set
+unisyl1_cross_ent = entropy_compute(test1_unisylprob, train_unisylprob, train1_unisyl_unkprob, dict = False)
+unisyl2_cross_ent = entropy_compute(test2_unisylprob, train_unisylprob, train2_unisyl_unkprob, dict = False)
+bisyl1_cross_ent = entropy_compute(test1_bisylprob, train_bisylprob, train1_bisyl_unkprob, dict = False)
 bisyl2_cross_ent = entropy_compute(test2_bisylprob, train_bisylprob, train2_bisyl_unkprob, dict = False)
 
+# jamo entropy
 unijamo_entropy = entropy_compute(train_unijamoprob, train_unijamoprob, dict = False)
 bijamo_entropy = entropy_compute(train_bijamoprob, train_bijamoprob, dict = False)
 test1_unijamo_entropy = entropy_compute(test1_unijamoprob, test1_unijamoprob, dict = False)
 test2_unijamo_entropy = entropy_compute(test2_unijamoprob, test2_unijamoprob, dict = False)
 test1_bijamo_entropy = entropy_compute(test1_bijamoprob, test1_bijamoprob, dict = False)
 test2_bijamo_entropy = entropy_compute(test2_bijamoprob, test2_bijamoprob, dict = False)
+
+# unk probability calculation
 train1_unijamo_unkprob = UNK_process(train_unijamoprob, test1_unijamoprob, train_unijamo_dict, test1_unijamo_dict)
-unijamo1_cross_ent = entropy_compute(test1_unijamoprob, train_unijamoprob, train1_unijamo_unkprob, dict = False)
 train2_unijamo_unkprob = UNK_process(train_unijamoprob, test2_unijamoprob, train_unijamo_dict, test2_unijamo_dict)
-unijamo2_cross_ent = entropy_compute(test2_unijamoprob, train_unijamoprob, train2_unijamo_unkprob, dict = False)
 train1_bijamo_unkprob = UNK_process(train_bijamoprob, test1_bijamoprob, train_bijamo_dict, test1_bijamo_dict)
-bijamo1_cross_ent = entropy_compute(test1_bijamoprob, train_bijamoprob, train1_bijamo_unkprob, dict = False)
 train2_bijamo_unkprob = UNK_process(train_bijamoprob, test2_bijamoprob, train_bijamo_dict, test2_bijamo_dict)
+
+# jamo cross entropy
+unijamo1_cross_ent = entropy_compute(test1_unijamoprob, train_unijamoprob, train1_unijamo_unkprob, dict = False)
+unijamo2_cross_ent = entropy_compute(test2_unijamoprob, train_unijamoprob, train2_unijamo_unkprob, dict = False)
+bijamo1_cross_ent = entropy_compute(test1_bijamoprob, train_bijamoprob, train1_bijamo_unkprob, dict = False)
 bijamo2_cross_ent = entropy_compute(test2_bijamoprob, train_bijamoprob, train2_bijamo_unkprob, dict = False)
 
+#### output entropy and cross-entropy as html files
 import pandas as pd # import library pandas
 df = pd.DataFrame({"Corpus": ["Sejong.nov.Traning", "", "", "", "Sejong.nov.test", "","","","Hani.test","","",""],
                   "Unit": [codecs.decode("자소별", "utf-8"), "", codecs.decode("음절별", "utf-8"), ""]*3,
